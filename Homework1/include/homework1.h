2ae5e90a1a6c2dfd670bbdedfb7dfa76b5d7ed0d
thocar <thocar@tzi.de>
2014-05-27 11:23:29 +0200

    style guide modifications

--- Homework1/include/homework1.h
+++ Homework1/include/homework1.h
@@ -1,30 +1,40 @@
-#ifndef HOMEWORK1
-#define HOMEWORK1
+#ifndef _HOMEWORK1_H_
+#define _HOMEWORK1_H_
 
 #include "gazebo/physics/physics.hh"
 #include "gazebo/gazebo.hh"
 
 namespace gazebo
 {
-class Homework1 : public SystemPlugin
-{
+	class Homework1 : public SystemPlugin
+	{
 
-public:
-    virtual void Load(int /*_argc*/, char ** /*_argv*/);
-    void OnUpdate(const common::UpdateInfo & /*_info*/);
+		public: virtual void Load(int /*_argc*/, char ** /*_argv*/);
+		/// \brief Callback method for updateConnection_, is called in every world update.
+		/// In this callback the objects "box1" and "sphere1" get some velocities to move.
+		public: void OnUpdate(const common::UpdateInfo & /*_info*/);
 
-private:
-    virtual void Init();
-    event::ConnectionPtr updateConnection_;
-    std::string world_name_;
-    physics::WorldPtr world_;
-    physics::PhysicsEnginePtr phy_eng_;
-    std::string phy_eng_name_;
-    int model_count_;
+		/// \brief This method initializes the plugin. At first the method gets acces to the current world and print the name.
+		/// Then the method gets acces to the physics engine and print out them too.
+		/// At last the methods subscribes on event::Events::ConnectWorldUpdateBegin.
+		private: virtual void Init();
+		
+		// attribute to update
+		private: event::ConnectionPtr _updateConnection;
+		// world ptr
+		private: physics::WorldPtr _world;
+		// world name
+		private: std::string _world_name;
+		// physics engine ptr
+		private: physics::PhysicsEnginePtr _phy_eng;
+		// physics engine name
+		private: std::string _phy_eng_name;
+		// model count
+		private: int _model_count;
 
-};
-// Register this plugin with the simulator
-GZ_REGISTER_SYSTEM_PLUGIN(Homework1)
+	};
+	// Register this plugin with the simulator
+	GZ_REGISTER_SYSTEM_PLUGIN(Homework1)
 }
 
 #endif
